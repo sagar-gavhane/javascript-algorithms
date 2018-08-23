be4e20fc4b7ba11482f294447e4deddcd40b3687
Sagar <sgavhane.dev@gmail.com>
2018-08-23 18:48:49 +0530

    functional prog, styled factorial function added

--- src/algorithms/math/factorial.js
+++ src/algorithms/math/factorial.js
@@ -8,4 +8,17 @@ function factorial(number) {
   return result;
 }
 
+/* Functional programming styled factorial algorithm */
+const natural = n => {
+  const arr0 = [...Array(n + 1).keys()];
+  const [first, ...arr] = arr0;
+  return arr;
+};
+
+function factorialFunctional(number) {
+  const list = number < 1 ? [1] : natural(number);
+  const multiply = (a, b) => a * b;
+  return list.reduce(multiply);
+}
+
 module.exports = factorial;
